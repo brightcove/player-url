5772b35b3808a7b24b4bb94dd16d64d9cfb3cb82
Pat O'Neill <pgoneill@gmail.com>
2018-08-15 11:28:49 -0400

    feat: Support whitelisted query string parameters for iframe URLs only. (#2)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,80 @@
 import {version as VERSION} from '../package.json';
 
+// The parameters that may include JSON.
+const JSON_ALLOWED_PARAMS = ['catalogSearch', 'catalogSequence'];
+
+// The parameters that may be set as query string parameters for iframes.
+const IFRAME_ALLOWED_QUERY_PARAMS = [
+  'applicationId',
+  'catalogSearch',
+  'catalogSequence',
+  'playlistId',
+  'playlistVideoId',
+  'videoId'
+];
+
+/**
+ * Gets the value of a parameter and encodes it as a string.
+ *
+ * For certain keys, JSON is allowed and will be encoded.
+ *
+ * @private
+ * @param   {Object} params
+ *          A parameters object. See README for details.
+ *
+ * @param   {string} key
+ *          The key in the params object.
+ *
+ * @return  {string|undefined}
+ *          The encoded value - or `undefined` if none.
+ */
+const getQueryParamValue = (params, key) => {
+
+  if (!params || params[key] === undefined) {
+    return;
+  }
+
+  // If it's not a string, such as with a catalog search or sequence, we
+  // try to encode it as JSON.
+  if (typeof params[key] !== 'string' && JSON_ALLOWED_PARAMS.indexOf(key) !== -1) {
+    try {
+      return encodeURIComponent(JSON.stringify(params[key]));
+    } catch (x) {
+
+      // If it's not a string and we can't encode as JSON, it's ignored entirely.
+      return;
+    }
+  }
+
+  return encodeURIComponent(String(params[key]).trim()) || undefined;
+};
+
+/**
+ * In some cases, we need to add query string parameters to an iframe URL.
+ *
+ * @private
+ * @param   {Object} params
+ *          An object of query parameters.
+ *
+ * @return  {string}
+ *          A query string starting with `?`. If no valid parameters are given,
+ *          returns an empty string.
+ */
+const getQueryString = (params) => {
+  return Object.keys(params)
+    .filter((k) => IFRAME_ALLOWED_QUERY_PARAMS.indexOf(k) !== -1)
+    .reduce((qs, k) => {
+      const value = getQueryParamValue(params, k);
+
+      if (value !== undefined) {
+        qs += (qs) ? '&' : '?';
+        qs += encodeURIComponent(k) + '=' + value;
+      }
+
+      return qs;
+    }, '');
+};
+
 /**
  * Generate a URL to a Brightcove Player.
  *
@@ -35,7 +110,8 @@ const brightcovePlayerUrl = ({
   playerId = 'default',
   embedId = 'default',
   iframe = false,
-  minified = true
+  minified = true,
+  queryParams = null
 }) => {
   let ext = '';
 
@@ -52,7 +128,13 @@ const brightcovePlayerUrl = ({
     base = base.substring(0, base.length - 1);
   }
 
-  return `${base}/${accountId}/${playerId}_${embedId}/index.${ext}`;
+  let qs = '';
+
+  if (iframe && queryParams && typeof queryParams === 'object') {
+    qs = getQueryString(queryParams);
+  }
+
+  return `${base}/${accountId}/${playerId}_${embedId}/index.${ext}${qs}`;
 };
 
 /**
--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,15 @@
 const tap = require('tap');
+const tsml = require('tsml');
 const bcpUrl = require('../dist/brightcove-player-url.cjs.js');
 
-tap.test('minimal arguments', (t) => {
+tap.test('minimal params', (t) => {
   const first = bcpUrl({accountId: '1'});
 
   t.equal(first, 'https://players.brightcove.net/1/default_default/index.min.js');
   t.end();
 });
 
-tap.test('argument: base', (t) => {
+tap.test('param: base', (t) => {
   const first = bcpUrl({accountId: '1', base: 'foo'});
   const second = bcpUrl({accountId: '1', base: 'bar/'});
 
@@ -17,21 +18,21 @@ tap.test('argument: base', (t) => {
   t.end();
 });
 
-tap.test('argument: playerId', (t) => {
+tap.test('param: playerId', (t) => {
   const first = bcpUrl({accountId: '1', playerId: 'foo'});
 
   t.equal(first, 'https://players.brightcove.net/1/foo_default/index.min.js');
   t.end();
 });
 
-tap.test('argument: embedId', (t) => {
+tap.test('param: embedId', (t) => {
   const first = bcpUrl({accountId: '1', embedId: 'foo'});
 
   t.equal(first, 'https://players.brightcove.net/1/default_foo/index.min.js');
   t.end();
 });
 
-tap.test('argument: minified', (t) => {
+tap.test('param: minified', (t) => {
   const first = bcpUrl({accountId: '1', minified: false});
   const second = bcpUrl({accountId: '1', minified: true});
 
@@ -40,7 +41,7 @@ tap.test('argument: minified', (t) => {
   t.end();
 });
 
-tap.test('argument: iframe', (t) => {
+tap.test('param: iframe', (t) => {
   const first = bcpUrl({accountId: '1', iframe: true});
   const second = bcpUrl({accountId: '1', iframe: true, minified: false});
   const third = bcpUrl({accountId: '1', iframe: true, minified: true});
@@ -49,4 +50,61 @@ tap.test('argument: iframe', (t) => {
   t.equal(second, 'https://players.brightcove.net/1/default_default/index.html');
   t.equal(third, 'https://players.brightcove.net/1/default_default/index.html');
   t.end();
+});
+
+tap.test('param: queryParams', (t) => {
+  const empty = bcpUrl({accountId: '1', iframe: true, queryParams: {}});
+  const unknown = bcpUrl({accountId: '1', iframe: true, queryParams: {foo: 'bar'}});
+  const inPage = bcpUrl({accountId: '1', iframe: false, queryParams: {videoId: '123'}});
+
+  const all = bcpUrl({
+    accountId: '1',
+    iframe: true,
+    queryParams: {
+      applicationId: 'a a',
+      catalogSearch: 'b',
+      catalogSequence: 'c',
+      playlistId: 'd',
+      playlistVideoId: 'e',
+      videoId: 'f',
+      ignored: 'NOPE'
+    }
+  });
+
+  const json = bcpUrl({
+    accountId: '1',
+    iframe: true,
+    queryParams: {
+      applicationId: {a: 1},
+      catalogSearch: {b: 2},
+      catalogSequence: {c: 3},
+      playlistId: {d: 4},
+      playlistVideoId: {e: 5},
+      videoId: {f: 6}
+    }
+  });
+
+  t.equal(empty, 'https://players.brightcove.net/1/default_default/index.html');
+  t.equal(unknown, 'https://players.brightcove.net/1/default_default/index.html');
+  t.equal(inPage, 'https://players.brightcove.net/1/default_default/index.min.js');
+
+  t.equal(all, tsml`
+    https://players.brightcove.net/1/default_default/index.html
+      ?applicationId=a%20a
+      &catalogSearch=b
+      &catalogSequence=c
+      &playlistId=d
+      &playlistVideoId=e
+      &videoId=f`);
+
+  t.equal(json, tsml`
+    https://players.brightcove.net/1/default_default/index.html
+      ?applicationId=%5Bobject%20Object%5D
+      &catalogSearch=%7B%22b%22%3A2%7D
+      &catalogSequence=%7B%22c%22%3A3%7D
+      &playlistId=%5Bobject%20Object%5D
+      &playlistVideoId=%5Bobject%20Object%5D
+      &videoId=%5Bobject%20Object%5D`);
+
+  t.done();
 });
